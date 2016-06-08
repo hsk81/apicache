f0d7e90ec2ffd69bcce11af64a4979d835a1c7ed
Hasan Karahan <hasan.karahan@blackhan.com>
2016-06-08 10:27:19 +0400

    IN-183: API request time flexibility increased

--- api_cache/view/api.py
+++ api_cache/view/api.py
@@ -21,19 +21,19 @@ app = app_api
 def api_rdb_n(rdb_index, path):
 
     rdb = getattr(api_cache.cache, 'redis_plugin_{0}'.format(rdb_index))
-    @rdb.memoize(expiry=ARGs.get('api_expiry'), name='view.api',
-                 unless=lambda: ARGs.debug())
+    idx = int(request.query.get('api-index', 0))
+    exp = request.query.get(
+        'api{0}-expiry'.format(idx), ARGs.get('api{0}_expiry'.format(idx)))
+
+    @rdb.memoize(expiry=exp, name='view.rdb', unless=lambda: ARGs.debug())
     def memoized(*args, **kwargs):
-        api_index = int(request.query.get('api-index', 0))
-        key_name = \
-            ARGs.get('api{0}_key_name'.format(api_index),
-                     request.query.get('api{0}-key-name'.format(api_index)))
-        key_value = \
-            ARGs.get('api{0}_key_value'.format(api_index),
-                     request.query.get('api{0}-key-value'.format(api_index)))
-        api_url = \
-            ARGs.get('api{0}_url'.format(api_index),
-                     request.query.get('api{0}-url'.format(api_index)))
+
+        key_name = request.query.get(
+            'api{0}-key-name'.format(idx), ARGs.get('api{0}_key_name'.format(idx)))
+        key_value = request.query.get(
+            'api{0}-key-value'.format(idx), ARGs.get('api{0}_key_value'.format(idx)))
+        api_url = request.query.get(
+            'api{0}-url'.format(idx), ARGs.get('api{0}_url'.format(idx)))
 
         if key_name and key_value: request.query[key_name] = key_value
         res = requests.get(os.path.join(api_url, path), params=request.query)
@@ -52,20 +52,20 @@ def api_rdb_i(path):
 @app.get('/mdb/<path:path>')
 def api_mdb_i(path):
 
-    mdb = getattr(api_cache.cache, 'memcached_plugin',)
-    @mdb.memoize(expiry=ARGs.get('api_expiry'), name='view.api',
-                 unless=lambda: ARGs.debug())
+    mdb = getattr(api_cache.cache, 'memcached_plugin')
+    idx = int(request.query.get('api-index', 0))
+    exp = request.query.get(
+        'api{0}-expiry'.format(idx), ARGs.get('api{0}_expiry'.format(idx)))
+
+    @mdb.memoize(expiry=exp, name='view.mdb', unless=lambda: ARGs.debug())
     def memoized(*args, **kwargs):
-        api_index = int(request.query.get('api-index', 0))
-        key_name = \
-            ARGs.get('api{0}_key_name'.format(api_index),
-                     request.query.get('api{0}-key-name'.format(api_index)))
-        key_value = \
-            ARGs.get('api{0}_key_value'.format(api_index),
-                     request.query.get('api{0}-key-value'.format(api_index)))
-        api_url = \
-            ARGs.get('api{0}_url'.format(api_index),
-                     request.query.get('api{0}-url'.format(api_index)))
+
+        key_name = request.query.get(
+            'api{0}-key-name'.format(idx), ARGs.get('api{0}_key_name'.format(idx)))
+        key_value = request.query.get(
+            'api{0}-key-value'.format(idx), ARGs.get('api{0}_key_value'.format(idx)))
+        api_url = request.query.get(
+            'api{0}-url'.format(idx), ARGs.get('api{0}_url'.format(idx)))
 
         if key_name and key_value: request.query[key_name] = key_value
         res = requests.get(os.path.join(api_url, path), params=request.query)
